a423fde53fd9d21ddd5604b49e598e671d44c150
Lukas Kral <lukywill16@gmail.com>
2022-11-17 02:47:17 +0100

    start for http server - for readiness and liveness probe
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

---
+++ canary/src/main/java/services/servers/HttpServerR.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package services.servers;
+
+import com.sun.net.httpserver.HttpServer;
+import services.health.HealthCheck;
+
+import java.net.InetSocketAddress;
+
+// TODO: different name
+public class HttpServerR {
+
+    public static void createHttpServer() throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/liveness", new HealthCheck.LivenessHandler());
+        server.createContext("/readiness", new HealthCheck.ReadinessHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+}
